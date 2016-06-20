2bd927813f4566bf4187fea8c92703ee21e56b23
maximburyak <maxim@ayende.com>
2016-06-20 18:47:59 +0300

    1

--- workingSimpleEcho.py
+++ workingSimpleEcho.py
@@ -1,28 +1,45 @@
 import tornado.websocket
 import tornado.concurrent
 import tornado.ioloop
+import tornado.httpclient
 from tornado.websocket import websocket_connect
 import functools
 import threading
 from multiprocessing import Process
 import time
+import json
+import random
 
-#class WebSocketParallelConnection(multiprocessing.Process):
-#	def __init__(self)
-#	multiproccessing.Proccess.__init__(self)
+class WebsocketStreamingConnectionManager:
 
-class WebsocketStreamingConnectionManager(threading.Thread):
-
-	def __init__(self, eventLoop, name):
+	def __init__(self, eventLoop, name,ip,port,database_name,start_etag,strategy):
 		threading.Thread.__init__(self)
 		self.eventLoop=eventLoop
 		self.name = name	
-	
+		self.ip = ip
+		self.port = port
+		self.database_name = database_name	
+		self.start_etag=start_etag
+		self.strategy=strategy
+
 	@tornado.gen.coroutine
 	def connect(self):
 		print ("started connection to thread %s", self.name)
 		#x = yield websocket_connect("ws://echo.websocket.org",io_loop = self.eventLoop)
-		x = yield websocket_connect("ws://192.168.56.1:57052/api/values",io_loop = self.eventLoop)
+		x = yield websocket_connect("ws://192.168.56.1:8080/databases/echo3/subscriptions/echo",io_loop = self.eventLoop)
+		print ("connected to thread %s", self.name)
+		return x
+
+	def subscription_connect(self):
+		createUrl = "http://"+self.ip+":"+self.port+"/databases/"+self.database_name+"/subscriptions/create?startEtag="+self.start_etag
+
+
+		http_client = tornado.httpclient.AsyncHTTPClient()
+		subscription_id = json.loads(yield http_client.fetch(createUrl))['Id']		
+		connection_id = random.randrange(1000000000)
+
+		wsUrl = "ws://"+self.ip+":"+self.port+"/databases/"+self.database_name+"/subscriptions/pull?id="+subscription_id + "?conneciton=" +connection_id+"&strategy="+strategy
+		x = yield websocket_connect(wsUrl,io_loop = self.eventLoop)
 		print ("connected to thread %s", self.name)
 		return x
 
@@ -43,100 +60,13 @@ class WebsocketStreamingConnectionManager(threading.Thread):
 		print ("%s was received as thread %s" % (response,self.name))		
 
 
-class WebsocketStreamingConnectionManager2(threading.Thread):
-
-	def __init__(self, eventLoop, name):
-		threading.Thread.__init__(self)
-		self.eventLoop=eventLoop
-		self.name = name	
-	
-	@tornado.gen.coroutine
-	def connect(self):
-		print ("started connection to thread %s", self.name)
-		x = yield websocket_connect("ws://echo.websocket.org",io_loop = self.eventLoop)
-		print ("connected to thread %s", self.name)
-		return x
-
-	@tornado.gen.coroutine
-	def receive(self):
-		x = yield self.conn.read_message()
-		return x
-
-
-	def run(self):
-		self.eventLoop.run_sync(self.innerRun)
-
-	@tornado.gen.coroutine
-	def innerRun(self):
-		print ("started run for thread %s", self.name)
-		self.conn = yield self.connect() #self.eventLoop.run_sync(self.connect)
-
-		msg = "Hello!"
-		self.conn.write_message(msg)
-		response = yield self.receive() # self.eventLoop.run_sync(self.receive)
-		print ("%s was received as thread %s" % (response,self.name))				
-
-
-def mainMultiThreaded():
-	wsThreads = []
-	for index in range(20):
-		newCon = WebsocketStreamingConnectionManager2(tornado.ioloop.IOLoop(),"Thread#"+str(index))
-		newCon.start()		
-		wsThreads.append(newCon)
-
-	print ("created threads")
-
-	for curThread in wsThreads:
-		curThread.join()
-
-	print ("started threads waiter")
-
-	for cur_thread in wsThreads:
-		cur_thread.join()		
-
-	print("end time: {0}".format((time.time() - startTime)))
-
-	
-
-
-class MyState:
-	def __init__(self, eventLoop, count):
-		self.count = count
-		self.my_lock = threading.Lock()
-		self.eventLoop = eventLoop
-	def dec(self,something):
-		with self.my_lock:
-			self.count = self.count -1
-			
-			if self.count ==0:
-				self.eventLoop.stop()
-		
-
 @tornado.gen.coroutine
-def main():
-	myAsyncState = MyState(tornado.ioloop.IOLoop.current(),20)
-
-	wsThreads = []
-	for index in range(20):
-		newCon = WebsocketStreamingConnectionManager(tornado.ioloop.IOLoop.current(),"Thread#"+str(index))
-		newCon.run(callback=myAsyncState.dec)
-		wsThreads.append(newCon)
-
-	print ("created threads")
-
-	print ("created threads waiter")
-
-	print ("started threads waiter")
-
-	print ("waiter on ioloop")
-
-@tornado.gen.coroutine
-def main3(count):
+def main(count):
 
 	wsThreads = []
 	some_children = []
 	for index in range(count):
-		newCon = WebsocketStreamingConnectionManager(tornado.ioloop.IOLoop.current(),"Thread#"+str(index))
+		newCon = WebsocketStreamingConnectionManager(tornado.ioloop.IOLoop.current(),"Thread#"+str(index),"192.168.56)
 		some_children.append(newCon.run())
 		wsThreads.append(newCon)
 
@@ -150,10 +80,10 @@ def main3(count):
 
 	print ("waiter on ioloop")
 
-def main3MPRunner():
+def mainMPRunner(processorsCount, requestsAmount):
 	proccesses = []
-	for index in range(4):
-		new_proccess = Process(target=functools.partial(tornado.ioloop.IOLoop().run_sync,functools.partial(main3,5)))
+	for index in range(proccessorsCount * 2):
+		new_proccess = Process(target=functools.partial(tornado.ioloop.IOLoop().run_sync,functools.partial(main,int(requestsAmount/(proccessorsCount*2)))))
 		new_proccess.start()
 		proccesses.append(new_proccess)
 	
@@ -166,10 +96,10 @@ def main3SPRunner():
 
 if __name__ == '__main__':
 	startTime = time.time()
-#	main()
-#	tornado.ioloop.IOLoop.current().start()
-#	tornado.ioloop.IOLoop.current().run_sync(main3)
-	main3MPRunner()
+	proccessorsCount = 4
+	requestsAmount = 1000
+#	tornado.ioloop.IOLoop.current().run_sync(functools.partial(main,1000))
+	mainMPRunner(proccessorsCount, requestsAmount)
 	print("end time: %s" % str(time.time() - startTime))
 
 #	mainMultiThreaded()
